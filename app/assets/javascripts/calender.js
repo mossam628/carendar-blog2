df6cdbda8bceedc374da96ed59e930686ec3e3ab
mossam628 <mosososo628@gmail.com>
2020-02-26 21:23:37 +0900

    event

--- app/assets/javascripts/calender.js
+++ app/assets/javascripts/calender.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
   $('#calendar').fullCalendar({
+    defaultDate: '2018-03-20',
+    events: [
+      {
+        title: '来客',
+        start: '2018-03-20'
+      },
+      {
+        title: '旅行',
+        start: '2018-03-25T06:00:00',
+        end: '2018-03-30T22:00:00'
+      }
+    ]
   })
   });
