86db969c6b1541221cf59c689ed06509e1e5a9ac
mossam628 <mosososo628@gmail.com>
2020-04-01 18:46:02 +0900

    commit

--- app/controllers/calender_controller.rb
+++ app/controllers/calender_controller.rb
@@ -1,6 +1,5 @@
 class CalenderController < ApplicationController
   def index
-    @user = User.find(params[:id])
     @events = Event.order(:id).limit(params[:limit]).offset(params[:offset])
     json = @events
     render json: json.to_json
